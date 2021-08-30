927648f6e3823d633465e3cec3290ce62c832306
Saecki <tobiasschmitz2001@gmail.com>
2021-08-30 05:32:01 +0200

    add lua lsp server

--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -208,10 +208,12 @@ let g:rust_recommended_style = 0
 
 " # crates.nvim
 " ------------------------------------------------------------
-nmap <leader>vt :lua require('crates').toggle()<cr>
-nmap <leader>vr :lua require('crates').reload()<cr>
-nmap <leader>vu :lua require('crates').update_crate()<cr>
-nmap <leader>vU :lua require('crates').upgrade_crate()<cr>
+nnoremap <silent> <leader>vt :lua require('crates').toggle()<cr>
+nnoremap <silent> <leader>vr :lua require('crates').reload()<cr>
+nnoremap <silent> <leader>vu :lua require('crates').update_crate()<cr>
+vnoremap <silent> <leader>vu :lua require('crates').update_crates()<cr>
+nnoremap <silent> <leader>vU :lua require('crates').upgrade_crate()<cr>
+vnoremap <silent> <leader>vU :lua require('crates').upgrade_crates()<cr>
 
 " # gitsigns.nvim
 " ------------------------------------------------------------
--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -31,6 +31,17 @@ local function setup_lsp_status()
     return lsp_status
 end
 
+local function setup_lsp_diagnostics()
+    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+        vim.lsp.diagnostic.on_publish_diagnostics, {
+            virtual_text = true,
+            signs = true,
+            update_in_insert = true,
+            severity_sort = true,
+        }
+    )
+end
+
 local function on_attach(client)
     require('lsp-status').on_attach(client)
 end
@@ -38,6 +49,7 @@ end
 local function setup()
     local capabilities = get_capabilities()
     local lsp_status = setup_lsp_status()
+    setup_lsp_diagnostics()
 
     local lsp_config = require('lspconfig')
     lsp_config.rust_analyzer.setup {
@@ -68,14 +80,37 @@ local function setup()
         },
     }
 
-    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-        vim.lsp.diagnostic.on_publish_diagnostics, {
-            virtual_text = true,
-            signs = true,
-            update_in_insert = true,
-            severity_sort = true,
-        }
-    )
+    local sumneko_root_path = vim.fn.expand('~/Projects/lua-language-server')
+    local sumneko_binary = sumneko_root_path.."/bin/Linux/lua-language-server"
+
+    local runtime_path = vim.split(package.path, ';')
+    table.insert(runtime_path, "lua/?.lua")
+    table.insert(runtime_path, "lua/?/init.lua")
+    lsp_config.sumneko_lua.setup {
+        cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+        settings = {
+            Lua = {
+                runtime = {
+                    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                    version = 'LuaJIT',
+                    -- Setup your lua path
+                    path = runtime_path,
+                },
+                diagnostics = {
+                    -- Get the language server to recognize the `vim` global
+                    globals = {'vim'},
+                },
+                workspace = {
+                    -- Make the server aware of Neovim runtime files
+                    library = vim.api.nvim_get_runtime_file("", true),
+                },
+                -- Do not send telemetry data containing a randomized but unique identifier
+                telemetry = {
+                    enable = false,
+                },
+            },
+        },
+    }
 end
 
 return {
