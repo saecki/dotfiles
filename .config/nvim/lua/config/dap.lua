14f1a42f70fa02de6b977dd46f58d0daa12fba1e
Saecki <tobiasschmitz2001@gmail.com>
2021-09-29 02:42:03 +0200

    add git aliases

--- .config/nvim/lua/config/dap.lua
+++ .config/nvim/lua/config/dap.lua
@@ -187,25 +187,25 @@ function M.setup()
     dap_ui.setup {
         icons = { expanded = "▾", collapsed = "▸" },
         sidebar = {
-          elements = {
-            { id = "scopes", size = 0.4, },
-            { id = "breakpoints", size = 0.3 },
-            { id = "stacks", size = 0.3 },
-          },
-          size = 80,
-          position = "left",
+            elements = {
+                { id = "scopes", size = 0.4, },
+                { id = "breakpoints", size = 0.3 },
+                { id = "stacks", size = 0.3 },
+            },
+            size = 80,
+            position = "left",
         },
         tray = {
-          elements = { "repl" },
-          size = 20,
-          position = "bottom",
+            elements = { "repl" },
+            size = 20,
+            position = "bottom",
         },
         floating = {
-          max_height = nil,
-          max_width = nil,
-          mappings = {
-            close = { "q", "<Esc>" },
-          },
+            max_height = nil,
+            max_width = nil,
+            mappings = {
+                close = { "q", "<Esc>" },
+            },
         },
         windows = { indent = 1 },
     }
