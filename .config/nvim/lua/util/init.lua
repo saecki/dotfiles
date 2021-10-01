a55ba40921e385fc0c0ebd9c9c4d11132e192484
Saecki <tobiasschmitz2001@gmail.com>
2021-10-01 20:45:28 +0200

    add missing neovim config file

---
+++ .config/nvim/lua/util/init.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+if jit ~= nil then
+    M.is_windows = jit.os == "Windows"
+else
+    M.is_windows = package.config:sub(1, 1) == "\\"
+end
+
+
+function M.path_separator()
+    if M.is_windows then
+        return '\\'
+    end
+    return '/'
+end
+
+function M.join_paths(...)
+    local separator = M.path_separator()
+    return table.concat({ ... }, separator)
+end
+
+return M
