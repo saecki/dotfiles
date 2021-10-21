98873e64181652ea90c340d7f0566fd8dc6218ae
Saecki <tobiasschmitz2001@gmail.com>
2021-10-21 16:28:25 +0200

    add snippets

--- .config/nvim/lua/config/luasnip.lua
+++ .config/nvim/lua/config/luasnip.lua
@@ -3,6 +3,7 @@ local M = {}
 local luasnip = require('luasnip')
 local s = luasnip.snippet
 local f = luasnip.function_node
+local t = luasnip.text_node
 
 local function bash(_, _, command)
 	local file = io.popen(command, "r")
@@ -15,10 +16,26 @@ end
 
 function M.setup()
     luasnip.snippets.all = {
-		s("pwd", f(bash, {}, "pwd")),
-		s("ls",  f(bash, {}, "ls")),
-		s("lsa",  f(bash, {}, "ls -A")),
-		s("date",  f(bash, {}, "date --iso-8601=date")),
+		s("pwd",      f(bash, {}, "pwd")),
+		s("ls",       f(bash, {}, "ls")),
+		s("lsa",      f(bash, {}, "ls -A")),
+		s("date",     f(bash, {}, "date '+%Y-%m-%d'")),
+		s("datetime", f(bash, {}, "date '+%Y-%m-%d %H:%M:%S'")),
+		s("time",     f(bash, {}, "date '+%H:%M:%S'")),
+
+        s("forall",      t("∀")),
+        s("exists",      t("∃")),
+        s("in",          t("∈")),
+        s("notin",       t("∉")),
+        s("subset",      t("⊆")),
+        s("subsetnoteq", t("⊂")),
+        s("intersect",   t("∩")),
+        s("union",       t("∪")),
+        s("setmin",      t("∖")),
+
+        s("land", t("∧")),
+        s("lor",  t("∨")),
+        s("xor",  t("⊻")),
     }
 end
 
