929f823be94e3805682e4591b7378653f78e325d
Saecki <tobiasschmitz2001@gmail.com>
2021-10-22 01:44:24 +0200

    update

--- .config/nvim/lua/config/luasnip.lua
+++ .config/nvim/lua/config/luasnip.lua
@@ -15,27 +15,29 @@ local function bash(_, _, command)
 end
 
 function M.setup()
-    luasnip.snippets.all = {
-		s("pwd",      f(bash, {}, "pwd")),
-		s("ls",       f(bash, {}, "ls")),
-		s("lsa",      f(bash, {}, "ls -A")),
-		s("date",     f(bash, {}, "date '+%Y-%m-%d'")),
-		s("datetime", f(bash, {}, "date '+%Y-%m-%d %H:%M:%S'")),
-		s("time",     f(bash, {}, "date '+%H:%M:%S'")),
+    luasnip.snippets = {
+        markdown = {
+            s("pwd",      f(bash, {}, "pwd")),
+            s("ls",       f(bash, {}, "ls")),
+            s("lsa",      f(bash, {}, "ls -A")),
+            s("date",     f(bash, {}, "date '+%Y-%m-%d'")),
+            s("datetime", f(bash, {}, "date '+%Y-%m-%d %H:%M:%S'")),
+            s("time",     f(bash, {}, "date '+%H:%M:%S'")),
 
-        s("forall",      t("∀")),
-        s("exists",      t("∃")),
-        s("in",          t("∈")),
-        s("notin",       t("∉")),
-        s("subset",      t("⊆")),
-        s("subsetnoteq", t("⊂")),
-        s("intersect",   t("∩")),
-        s("union",       t("∪")),
-        s("setmin",      t("∖")),
+            s("forall",      t("∀")),
+            s("exists",      t("∃")),
+            s("in",          t("∈")),
+            s("notin",       t("∉")),
+            s("subset",      t("⊆")),
+            s("subsetnoteq", t("⊂")),
+            s("intersect",   t("∩")),
+            s("union",       t("∪")),
+            s("setmin",      t("∖")),
 
-        s("land", t("∧")),
-        s("lor",  t("∨")),
-        s("xor",  t("⊻")),
+            s("land", t("∧")),
+            s("lor",  t("∨")),
+            s("xor",  t("⊻")),
+        }
     }
 end
 
