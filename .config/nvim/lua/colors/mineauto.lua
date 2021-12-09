ac7f6cc47b458b1c48f323edf73a2d04b0320621
Saecki <tobiasschmitz2001@gmail.com>
2021-12-09 18:06:30 +0100

    WIP starship prompt theme update neovim config for new alco version

--- .config/nvim/lua/colors/mineauto.lua
+++ .config/nvim/lua/colors/mineauto.lua
@@ -1,11 +1,27 @@
-local function file_exists(path)
-    return io.open(path, "r") ~= nil
+local function read_current_colorscheme(path)
+    local file = io.open(path, "r")
+    if not file then
+        return nil
+    end
+    local current = nil
+
+    while current == nil do
+        local text = file:read("*l")
+        if text == nil then
+            break
+        end
+        current = text:match("^%s*current: (.*)$")
+    end
+    file:close()
+
+    return current
 end
 
-local dir = vim.env.HOME.."/.config/alacritty/colors/current"
-if file_exists(dir.."/minedark.yml") then
+local dir = vim.env.HOME.."/.config/alco/alco.yml"
+local current = read_current_colorscheme(dir)
+if current == "minedark" then
     return require('colors.minedark')
-elseif file_exists(dir.."/minelight.yml") then
+elseif current == "minelight" then
     return require('colors.minelight')
 else
     return require('colors.minedark')
