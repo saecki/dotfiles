ed2490db1ad104219f72525bcd881df44deeee2a
Saecki <tobiasschmitz2001@gmail.com>
2021-12-17 18:27:28 +0100

    nvim: override some devicons

--- .config/nvim/lua/config/devicons.lua
+++ .config/nvim/lua/config/devicons.lua
@@ -4,6 +4,28 @@ local devicons = require("nvim-web-devicons")
 
 function M.setup()
     devicons.set_default_icon("", "#6d8086")
+    local git = {
+        icon = "",
+        color = "#f14c28",
+        cterm_color = "202",
+        name = "Git",
+    }
+    devicons.setup({
+        override = {
+            ["Dockerfile"] = {
+                icon = "",
+                color = "#497dc7",
+                cterm_color = "59",
+                name = "Dockerfile",
+            },
+            ["git"] = git,
+            [".gitattributes"] = git,
+            [".gitconfig"] = git,
+            [".gitignore"] = git,
+            [".gitmodules"] = git,
+            ["COMMIT_EDITMSG"] = git,
+        },
+    })
 end
 
 return M
