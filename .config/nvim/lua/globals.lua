c00b43ba55ec3e0a044b42b9e70cea7cc34bb791
Saecki <tobiasschmitz2001@gmail.com>
2021-12-23 20:26:52 +0100

    fix(nvim): global P function

--- .config/nvim/lua/globals.lua
+++ .config/nvim/lua/globals.lua
@@ -1,6 +1,6 @@
 function P(...)
-    local args = {...}
-    for _,a in pairs(args) do
-        print(vim.inspect(a))
+    local args = table.pack(...)
+    for i = 1, args.n, 1 do
+        print(vim.inspect(args[i]))
     end
 end
