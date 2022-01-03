b35c4df1b1cbc229bce3bba0270c6a18935018ff
Saecki <tobiasschmitz2001@gmail.com>
2022-01-03 02:48:02 +0100

    refactor(nvim): remove null-ls gitsigns codeactions

--- .config/nvim/lua/config/null-ls.lua
+++ .config/nvim/lua/config/null-ls.lua
@@ -8,7 +8,6 @@ function M.setup()
         on_attach = lsp_config.on_attach,
         sources = {
             null_ls.builtins.diagnostics.teal,
-            null_ls.builtins.code_actions.gitsigns,
             null_ls.builtins.formatting.stylua,
         },
     })
