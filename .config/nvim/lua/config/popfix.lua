519c0522236cd2a1683c47b4a222e673bc9ccf6a
Saecki <tobiasschmitz2001@gmail.com>
2022-01-03 02:48:43 +0100

    refactor(nvim): cleanup vim.ui.select implementation

--- .config/nvim/lua/config/popfix.lua
+++ .config/nvim/lua/config/popfix.lua
@@ -38,60 +38,51 @@ local custom_ui_select = function(items, opts, on_choice)
     local commit_choice = function(choice_index)
         on_choice(items[choice_index], choice_index)
     end
-    local select_index = function(num)
-        return function(popup)
+    local formatted_entries = format_entries(items, opts.format_item)
+    local keymaps = {
+        i = {
+            ["<Cr>"] = function(popup)
+                popup:close(function(sel)
+                    commit_choice(sel)
+                end)
+                popup_reference = nil
+            end,
+        },
+        n = {
+            ["<Cr>"] = function(popup)
+                popup:close(function(sel)
+                    commit_choice(sel)
+                end)
+                popup_reference = nil
+            end,
+            ["<C-o>"] = function(popup)
+                popup:close()
+                popup_reference = nil
+            end,
+            ["<Esc>"] = function(popup)
+                popup:close()
+                popup_reference = nil
+            end,
+            ["q"] = function(popup)
+                popup:close()
+                popup_reference = nil
+            end,
+        },
+    }
+    for i = 1, #formatted_entries do
+        keymaps.n[tostring(i)] = function(popup)
             popup:close(function()
-                commit_choice(num)
+                commit_choice(i)
             end)
             popup_reference = nil
         end
     end
 
-    local formatted_entries = format_entries(items, opts.format_item)
-
     popup_reference = popfix:new({
         width = calculate_popup_width(formatted_entries),
         height = #formatted_entries,
         close_on_bufleave = true,
-        keymaps = {
-            i = {
-                ["<Cr>"] = function(popup)
-                    popup:close(function(sel)
-                        commit_choice(sel)
-                    end)
-                    popup_reference = nil
-                end,
-            },
-            n = {
-                ["<Cr>"] = function(popup)
-                    popup:close(function(sel)
-                        commit_choice(sel)
-                    end)
-                    popup_reference = nil
-                end,
-                ["<C-o>"] = function(popup)
-                    popup:close()
-                    popup_reference = nil
-                end,
-                ["<Esc>"] = function(popup)
-                    popup:close()
-                    popup_reference = nil
-                end,
-                ["q"] = function(popup)
-                    popup:close()
-                    popup_reference = nil
-                end,
-                ["1"] = select_index(1),
-                ["2"] = select_index(2),
-                ["3"] = select_index(3),
-                ["4"] = select_index(4),
-                ["5"] = select_index(5),
-                ["6"] = select_index(6),
-                ["7"] = select_index(7),
-                ["8"] = select_index(8),
-                ["9"] = select_index(9),
-            },
-        },
+        keymaps = keymaps,
         callbacks = {
             close = function()
                 popup_reference = nil
