b5a9ab739f97e694ec19bbccedbdcb7f38c79b14
Saecki <tobiasschmitz2001@gmail.com>
2022-01-12 13:45:23 +0100

    feat(nvim): safely call top-level config modules

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,23 @@
+local function prequire(name, setup)
+    local ok, mod = pcall(require, name)
+    if not ok then
+        vim.notify("Failed to load config module " .. name, vim.log.levels.ERROR)
+        return
+    end
+
+    if setup ~= false then
+        mod.setup()
+    end
+
+    return mod
+end
+
 pcall(require, "impatient")
 
-require("globals")
-require("util.maps").setup()
-require("options").setup()
-require("mappings").setup()
-require("colors").setup()
-require("diagnostic").setup()
-require("plugins").setup()
+prequire("globals", false)
+prequire("util.maps")
+prequire("options")
+prequire("mappings")
+prequire("colors")
+prequire("diagnostic")
+prequire("plugins")
