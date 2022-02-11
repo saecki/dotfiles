d8216f0f5996cf7a68b8684a2254cf02ad0a996a
Saecki <tobiasschmitz2001@gmail.com>
2022-02-11 11:48:04 +0100

    feat(nvim): toggle 3 way diff

--- .config/nvim/lua/config/fugitive.lua
+++ .config/nvim/lua/config/fugitive.lua
@@ -2,11 +2,21 @@ local M = {}
 
 local wk = require("which-key")
 
+local function toggle_diff()
+    if vim.o.diff then
+        vim.cmd("diffoff")
+        vim.cmd("bdelete //2")
+        vim.cmd("bdelete //3")
+    else
+        vim.cmd("Gvdiffsplit!")
+    end
+end
+
 function M.setup()
     wk.register({
         ["<leader>g"] = {
             name = "Git",
-            ["d"] = { ":Gvdiffsplit!<cr>", "Open 3 way diff" },
+            ["d"] = { toggle_diff, "Toggle 3 way diff" },
             ["h"] = { ":diffget //2<cr>", "Get left diff" },
             ["l"] = { ":diffget //3<cr>", "Get right diff" },
         }
