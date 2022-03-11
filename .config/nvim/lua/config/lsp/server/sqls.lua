46786f818d0371f02820d39bd7d0dfc048a7061b
Saecki <tobiasschmitz2001@gmail.com>
2022-03-11 16:42:21 +0100

    feat(nvim): add sqls key mappings

--- .config/nvim/lua/config/lsp/server/sqls.lua
+++ .config/nvim/lua/config/lsp/server/sqls.lua
@@ -1,11 +1,24 @@
 local M = {}
 
 local sqls = require("sqls")
+local wk = require("which-key")
 
 function M.setup(server, on_init, on_attach, capabilities)
     local function m_on_attach(client, buf)
         sqls.on_attach(client, buf)
         on_attach(client, buf)
+
+        wk.register({
+            ["<leader>q"] = { "<Plug>(sqls-execute-query)", "Execute query" },
+        }, {
+            buffer = buf,
+        })
+        wk.register({
+            ["<leader>q"] = { "<Plug>(sqls-execute-query)", "Execute selected query" },
+        }, {
+            buffer = buf,
+            mode = "x",
+        })
     end
 
     server:setup({
