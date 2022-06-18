931ea179962619915ea2fa3fd0e5a9d997ffb38e
Saecki <tobiasschmitz2001@gmail.com>
2022-06-18 15:17:08 +0200

    refactor(nvim): update nvim-dap-ui config

--- .config/nvim/lua/config/dap/init.lua
+++ .config/nvim/lua/config/dap/init.lua
@@ -25,19 +25,21 @@ function M.setup()
 
     dap_ui.setup({
         icons = { expanded = "▾", collapsed = "▸" },
-        sidebar = {
-            elements = {
-                { id = "scopes", size = 0.4 },
-                { id = "breakpoints", size = 0.3 },
-                { id = "stacks", size = 0.3 },
+        layouts = {
+            {
+                elements = {
+                    { id = "scopes", size = 0.4 },
+                    { id = "breakpoints", size = 0.3 },
+                    { id = "stacks", size = 0.3 },
+                },
+                size = 80,
+                position = "left",
             },
-            size = 80,
-            position = "left",
-        },
-        tray = {
-            elements = { "repl" },
-            size = 20,
-            position = "bottom",
+            {
+                elements = { "repl" },
+                size = 20,
+                position = "bottom",
+            },
         },
         floating = {
             max_height = nil,
