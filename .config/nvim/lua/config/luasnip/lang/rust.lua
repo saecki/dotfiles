7f0c95b306dec6dff967bf96c1ba4f9a17ec3b6d
Saecki <tobiasschmitz2001@gmail.com>
2022-07-23 22:30:11 +0200

    feat(nvim): add rust snippet

--- .config/nvim/lua/config/luasnip/lang/rust.lua
+++ .config/nvim/lua/config/luasnip/lang/rust.lua
@@ -2,6 +2,7 @@ local luasnip = require("luasnip")
 local s = luasnip.snippet
 local t = luasnip.text_node
 local i = luasnip.insert_node
+local f = luasnip.function_node
 
 return {
     s("stateful_atom", {
@@ -12,6 +13,19 @@ return {
         t({ "\tstate: State::Existing(bounds),", "" }),
         t({ "\t..Default::default()", "" }),
         t({ "};" }),
+        i(0),
+    }),
+    s("accessor", {
+        t({ "pub fn " }),
+        i(1, { "ident" }),
+        t({ "(&self) -> f64 {", "" }),
+        t({ "\tself." }),
+        f(
+            function(args) return args[1][1] end,
+            {1}
+        ),
+        t({ " as f64", "" }),
+        t({ "}" }),
         i(0),
     }),
 }
