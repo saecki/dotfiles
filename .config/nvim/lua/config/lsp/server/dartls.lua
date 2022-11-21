37ddf3bd4192e13e3a19bbbae27292ff74a565ea
Saecki <tobiasschmitz2001@gmail.com>
2022-11-21 22:44:53 +0100

    fix(nvim): use 0 when bufnr passed to handler is nil

--- .config/nvim/lua/config/lsp/server/dartls.lua
+++ .config/nvim/lua/config/lsp/server/dartls.lua
@@ -9,13 +9,15 @@ local function closing_labels_handler(err, result, ctx)
         return
     end
 
-    vim.api.nvim_buf_clear_namespace(ctx.bufnr, namespace, 0, -1)
+    local bufnr = ctx.bufnr or 0
+
+    vim.api.nvim_buf_clear_namespace(bufnr, namespace, 0, -1)
 
     local prefix = "// "
     local highlight = "NonText"
     for _, item in ipairs(result.labels) do
         local line = item.range["end"].line
-        vim.api.nvim_buf_set_extmark(ctx.bufnr, namespace, tonumber(line), -1, {
+        vim.api.nvim_buf_set_extmark(bufnr, namespace, tonumber(line), -1, {
             virt_text = { {
                 prefix .. item.label,
                 highlight,
