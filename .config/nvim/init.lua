fda78b83178cf83418b36e9a25dbaecbcd4b0ac3
Saecki <tobiasschmitz2001@gmail.com>
2022-12-16 22:35:15 +0100

    feat(nvim): print module error message

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,21 +1,28 @@
+local function error_handler(err)
+    return err
+end
+
 local function prequire(name, setup)
-    local ok, mod = pcall(require, name)
+    local ok, mod = xpcall(function()
+        return require(name)
+    end, error_handler)
     if not ok then
-        vim.notify("Failed to load config module " .. name, vim.log.levels.ERROR)
+        vim.notify("Failed to load config module " .. name, vim.log.levels.ERROR .. "\n" .. (mod or ""))
         return
     end
 
     if setup ~= false then
-        local ok = pcall(mod.setup)
+        local ok, res = xpcall(mod.setup, error_handler)
+
         if not ok then
-            vim.notify("Failed to setup config module " .. name, vim.log.levels.ERROR)
+            vim.notify("Failed to setup config module " .. name, vim.log.levels.ERROR .. "\n" .. res)
         end
     end
 
     return mod
 end
 
-pcall(require, "impatient")
+prequire("impatient", false)
 
 prequire("globals", false)
 prequire("util.select")
