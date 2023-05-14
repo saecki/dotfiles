83fcfdad83fbe52e2aafb0793b9e1dad00100546
Saecki <tobiasschmitz2001@gmail.com>
2023-05-14 03:54:08 +0200

    fix(nvim): migrate nvim-tree mappings to on_attach

--- .config/nvim/lua/config/nvim-tree.lua
+++ .config/nvim/lua/config/nvim-tree.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 local nvim_tree = require("nvim-tree")
+local api = require("nvim-tree.api")
 local wk = require("which-key")
 local util = require("util")
 
@@ -52,49 +53,54 @@ function M.setup()
         },
         view = {
             width = 40,
-            mappings = {
-                list = {
-                    { key = { "<cr>", "o", "<2-leftmouse>" },   action = "edit" },
-                    { key = { "<2-rightmouse>", "<c-]>", "$" }, action = "cd" },
-                    { key = "<c-v>",                            action = "vsplit" },
-                    { key = "<c-x>",                            action = "split" },
-                    { key = "<c-t>",                            action = "tabnew" },
-                    { key = "<",                                action = "prev_sibling" },
-                    { key = ">",                                action = "next_sibling" },
-                    { key = "P",                                action = "parent_node" },
-                    { key = "<bs>",                             action = "close_node" },
-                    { key = "<s-cr>",                           action = "close_node" },
-                    { key = "<tab>",                            action = "preview" },
-                    { key = "K",                                action = "first_sibling" },
-                    { key = "J",                                action = "last_sibling" },
-                    { key = "I",                                action = "toggle_ignored" },
-                    { key = "<c-h>",                            action = "toggle_dotfiles" },
-                    { key = "<c-r>",                            action = "refresh" },
-                    { key = "a",                                action = "create" },
-                    { key = "d",                                action = "remove" },
-                    { key = "r",                                action = "rename" },
-                    { key = "R",                                action = "full_rename" },
-                    { key = "x",                                action = "cut" },
-                    { key = "c",                                action = "copy" },
-                    { key = "p",                                action = "paste" },
-                    { key = "y",                                action = "copy_name" },
-                    { key = "Y",                                action = "copy_path" },
-                    { key = "gy",                               action = "copy_absolute_path" },
-                    { key = "g{",                               action = "prev_git_item" },
-                    { key = "g}",                               action = "next_git_item" },
-                    { key = "-",                                action = "dir_up" },
-                    { key = "s",                                action = "system_open" },
-                    { key = "q",                                action = "close" },
-                    { key = "g?",                               action = "toggle_help" },
-                },
-            },
         },
+        on_attach = function(buf)
+            wk.register({
+                ["<cr>"]           = { api.node.open.edit, "Open" },
+                ["o"]              = { api.node.open.edit, "Open" },
+                ["<2-leftmouse>"]  = { api.node.open.edit, "Open" },
+                ["<2-rightmouse>"] = { api.tree.change_root_to_node, "cd" },
+                ["<c-]>"]          = { api.tree.change_root_to_node, "cd" },
+                ["<c-v>"]          = { api.node.open.vertical, "Open vertical split" },
+                ["<c-x>"]          = { api.node.open.horizontal, "Open horizontal split" },
+                ["<c-t>"]          = { api.node.open.tab, "Open in new tab" },
+                ["<"]              = { api.node.navigate.sibling.prev, "Next sibling" },
+                [">"]              = { api.node.navigate.sibling.next, "Prev sibling" },
+                ["K"]              = { api.node.navigate.sibling.first, "First sibling" },
+                ["J"]              = { api.node.navigate.sibling.last, "Last sibling" },
+                ["P"]              = { api.node.navigate.parent, "Parent directory" },
+                ["-"]              = { api.node.navigate.parent, "Parent directory" },
+                ["<bs>"]           = { api.node.navigate.parent_close, "Close directory" },
+                ["I"]              = { api.tree.toggle_gitignore_filter, "Toggle gitignore" },
+                ["<c-h>"]          = { api.tree.toggle_hidden_filter, "Toggle hidden files" },
+                ["<c-r>"]          = { api.tree.reload, "Refresh" },
+                ["a"]              = { api.fs.create, "Create" },
+                ["d"]              = { api.fs.remove, "Remove" },
+                ["r"]              = { api.fs.rename_basename, "Rename file stem" },
+                ["R"]              = { api.fs.rename, "Rename filename" },
+                ["x"]              = { api.fs.cut, "Cut file" },
+                ["p"]              = { api.fs.paste, "Paste file" },
+                ["c"]              = { api.fs.copy.node, "Copy file" },
+                ["y"]              = { api.fs.copy.filename, "Copy filename" },
+                ["Y"]              = { api.fs.copy.relative_path, "Copy relative path" },
+                ["gy"]             = { api.fs.copy.absolute_path, "Copy absolute path" },
+                ["g["]             = { api.node.navigate.diagnostics.prev, "Next diagnsotic" },
+                ["g]"]             = { api.node.navigate.diagnostics.next, "Prev diagnsotic" },
+                ["g{"]             = { api.node.navigate.git.prev, "Next git item" },
+                ["g}"]             = { api.node.navigate.git.next, "Prev git item" },
+                ["s"]              = { api.node.run.system, "Open system" },
+                ["q"]              = { api.tree.close, "Close" },
+                ["g?"]             = { api.toggle_help, "Toggle help" },
+            }, {
+                buffer = buf,
+            })
+        end
     }
     -- stylua: ignore end
 
     wk.register({
-        ["<leader>x"] = { nvim_tree.toggle, "Filetree toggle" },
-        ["<leader>X"] = { util.wrap(nvim_tree.toggle, true), "Filetree current file" },
+        ["<leader>x"] = { api.tree.toggle, "Filetree toggle" },
+        ["<leader>X"] = { util.wrap(api.tree.toggle, { find_file = true }), "Filetree current file" },
     })
 end
 
