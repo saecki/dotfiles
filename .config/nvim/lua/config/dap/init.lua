458042ffb2c21db147d982631762ffbeba299713
Saecki <tobiasschmitz2001@gmail.com>
2023-09-12 14:50:22 +0200

    feat!(nvim): don't automatically close dap-ui when session ends

--- .config/nvim/lua/config/dap/init.lua
+++ .config/nvim/lua/config/dap/init.lua
@@ -65,12 +65,12 @@ function M.setup()
     dap.listeners.after.event_initialized["dapui_config"] = function()
         dapui.open()
     end
-    dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-    end
-    dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-    end
+    -- dap.listeners.before.event_terminated["dapui_config"] = function()
+    --     dapui.close()
+    -- end
+    -- dap.listeners.before.event_exited["dapui_config"] = function()
+    --     dapui.close()
+    -- end
 
     wk.register({
         ["<f9>"] = { dap.continue, "Debug continue" },
