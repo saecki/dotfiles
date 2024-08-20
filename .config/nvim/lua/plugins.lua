ea273a7731835c3d97d4dea814921168159284ff
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2024-08-20 11:33:41 +0200

    feat(nvim): [WIP] write simple plugin manager

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,251 +1,64 @@
+local pm = require("plugman")
+
 local M = {}
 
-local mini_package_path = vim.fn.stdpath("data") .. "/site/"
-local mini_package_start_path = mini_package_path .. "pack/deps/start/"
-local mini_deps_path = mini_package_start_path .. "mini.deps"
-
-local native_notify = vim.notify
-
-local function print_info(pattern, ...)
-    native_notify(string.format(pattern, ...), vim.log.levels.INFO)
-    vim.cmd.redraw()
-end
-
-local function print_error(pattern, ...)
-    native_notify(string.format(pattern, ...), vim.log.levels.ERROR)
-    vim.cmd.redraw()
-end
-
-local function rm_rf(path)
-    local res = nil
-    vim.system({"rm", "-rf", path}, {}, function(r)
-        res = r
-    end)
-    vim.wait(100, function()
-        return res ~= nil
-    end, 1)
-    if res and res.code == 0 then
-        print_info("removed `%s`", path)
-    else
-        print_error("error removing `%s`: %s", path, res.stderr)
-        error()
-    end
-end
-
-local function unlink(path, prev_link)
-    local ok, err = vim.uv.fs_unlink(path)
-    if ok then
-        print_info("unlinked `%s` -> `%s`", path, prev_link)
-    else
-        print_error("error unlinking `%s` -> `%s`: %s", path, prev_link, err)
-        error()
-    end
-end
-
-local function symlink(link, link_target)
-    local ok, err = vim.uv.fs_symlink(link_target, link, { dir = true })
-    if ok then
-        print_info("created symlink `%s` -> `%s`", link, link_target)
-    else
-        print_error("error creating symlink from `%s` -> `%s`: %s", link, link_target, err)
-        error()
-    end
-end
-
----@param command string[]
----@param cwd string?
-local function run_command(command, cwd)
-    local command_str = table.concat(command, " ")
-    print_info("running `%s`", command_str)
-    vim.cmd.redraw()
-
-    local res = nil
-    local timeout = 30000
-    local opts = {
-        timeout = timeout,
-        cwd = cwd,
-    }
-    vim.system(command, opts, function(r)
-        res = r
-    end)
-    vim.wait(timeout, function()
-        return res ~= nil
-    end, 100)
-
-    if res and res.code == 0 then
-        print_info("success `%s`", command_str)
-    else
-        print_error("error running `%s`: `%s`", command_str, res.stderr)
-        error()
-    end
-end
-
----@param spec table|string
----@return string
-local function dev_repo(spec)
-    if type(spec) == "string" then
-        spec = { source = spec }
-    end
-    repo_url = "git@github.com:" .. spec.source
-
-    local last_component = string.match(repo_url, "/?([^/]+)$")
-    local project_path = vim.fn.expand("~/Projects/" .. last_component)
-    local package_path = mini_package_path .. "pack/deps/opt/" .. last_component
-
-    local function ensure_installed()
-        if not vim.uv.fs_stat(project_path) then
-            run_command({ "git", "clone", "--quiet", repo_url, project_path })
-            if spec.checkout then
-                run_command({ "git", "checkout", "--quiet", spec.checkout }, project_path)
-            end
-        end
-
-        if vim.uv.fs_stat(package_path) then
-            local link_path = vim.uv.fs_readlink(package_path)
-            if link_path == project_path then
-                -- correct symlink already exists
-            elseif link_path then
-                -- incorrect symlink
-                unlink(package_path, project_path)
-                symlink(package_path, project_path)
-            else
-                -- not a symlink
-                rm_rf(package_path)
-                symlink(package_path, project_path)
-            end
-        else
-            symlink(package_path, project_path)
-        end
-    end
-    pcall(ensure_installed)
-
-    return "file://" .. project_path
-end
-
----@param name string?
----@param keys string[]
-local function setup_on_keys(name, keys)
-    local loaded = false
-    local function load_and_feedkeys(key)
-        return function()
-            if loaded then
-                return
-            end
-            loaded = true
-
-            -- delete lazy loading keymaps
-            for _, lhs in ipairs(keys) do
-                vim.keymap.del("n", lhs)
-            end
-
-            require("config." .. name).setup()
-
-            local feed = vim.api.nvim_replace_termcodes("<ignore>" .. key, true, true, true)
-            vim.api.nvim_feedkeys(feed, "i", false)
-        end
-    end
-
-    for _, lhs in ipairs(keys) do
-        vim.keymap.set("n", lhs, load_and_feedkeys(lhs), {})
-    end
-end
-
 function M.setup()
-    -- bootstrap mini.deps
-    if not vim.uv.fs_stat(mini_deps_path) then
-        print_info("installing `mini.deps`")
-        local clone_cmd = {
-            "git", "clone", "--filter=blob:none",
-            "https://github.com/echasnovski/mini.deps", mini_deps_path
-        }
-        vim.fn.system(clone_cmd)
-        vim.cmd("packadd mini.deps | helptags ALL")
-        print_info("installed `mini.deps`")
-    end
-
-    local mini_deps = require('mini.deps')
-    mini_deps.setup({
-        path = {
-            package = mini_package_path,
-            snapshot = vim.fn.stdpath("config") .. "/mini-deps.lock",
-        },
-    })
-
-    local setup_queue = {}
-
-    ---@param name string?
-    ---@param spec table|string
-    local function add(name, spec)
-        if type(spec) == "string" then
-            spec = { source = spec }
-        end
-        mini_deps.add(spec)
-
-        if name then
-            table.insert(setup_queue, "config." .. name)
-        end
-    end
+    pm.create_dirs()
 
     -- key mappings
-    add("which-key", "folke/which-key.nvim")
+    pm.add("which-key", "folke/which-key.nvim")
 
     -- load nvim-notify first so errors are pretty
-    add("notify", "rcarriga/nvim-notify")
+    pm.add("notify", "rcarriga/nvim-notify")
 
     -- gui enhancements
-    add("lualine", {
-        source = "nvim-lualine/lualine.nvim",
-        depends = {
-            "smoka7/multicursors.nvim",
-            "smoka7/hydra.nvim",
-        },
-    })
-    add("mini_hipatterns", "echasnovski/mini.hipatterns")
-    add("indent-blankline", "lukas-reineke/indent-blankline.nvim")
-    add("devicons", "nvim-tree/nvim-web-devicons")
+    pm.add("lualine", "nvim-lualine/lualine.nvim")
+    pm.add("mini_hipatterns", "echasnovski/mini.hipatterns")
+    pm.add("indent-blankline", "lukas-reineke/indent-blankline.nvim")
+    pm.add("devicons", "nvim-tree/nvim-web-devicons")
 
     -- multicursor
-    add(nil, "mg979/vim-visual-multi")
+    pm.add(nil, "mg979/vim-visual-multi")
 
     -- filetree
-    add(nil, {
+    pm.add(nil, {
         source = "kyazdani42/nvim-tree.lua",
-        depends = { "nvim-tree/nvim-web-devicons" },
+        deps = { "nvim-tree/nvim-web-devicons" },
     })
-    setup_on_keys("nvim-tree", { "<leader>x", "<leader>X" })
+    pm.setup_on_keys("nvim-tree", { "<leader>x", "<leader>X" })
 
     -- file search/replace
-    add("spectre", {
+    pm.add("spectre", {
         source = "nvim-pack/nvim-spectre",
-        depends = {
+        deps = {
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons",
         },
     })
 
     -- substitute
-    add("substitute", "gbprod/substitute.nvim")
+    pm.add("substitute", "gbprod/substitute.nvim")
 
     -- file navigation
-    add(nil, "farmergreg/vim-lastplace")
-    add("telescope", {
+    pm.add(nil, "farmergreg/vim-lastplace")
+    pm.add("telescope", {
         source = "nvim-telescope/telescope.nvim",
-        depends = {
+        deps = {
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons",
         },
     })
 
     -- lists
-    add("trouble", {
+    pm.add("trouble", {
         source = "folke/trouble.nvim",
-        depends = { "folke/todo-comments.nvim" },
+        deps = { "folke/todo-comments.nvim" },
     })
 
     -- completion
-    add("cmp", {
+    pm.add("cmp", {
         source = "hrsh7th/nvim-cmp",
-        depends = {
+        deps = {
             "hrsh7th/cmp-path",
             "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-nvim-lsp",
@@ -255,17 +68,17 @@ function M.setup()
     })
 
     -- snippets
-    add("luasnip", "L3MON4D3/LuaSnip")
+    pm.add("luasnip", "L3MON4D3/LuaSnip")
 
     -- git
-    add("fugitive", "tpope/vim-fugitive")
-    add("gitsigns", "lewis6991/gitsigns.nvim")
+    pm.add("fugitive", "tpope/vim-fugitive")
+    pm.add("gitsigns", "lewis6991/gitsigns.nvim")
 
     -- lsp
-    add("lsp", {
+    pm.dev_repo(nil, "saecki/live-rename.nvim")
+    pm.add("lsp", {
         source = "neovim/nvim-lspconfig",
-        depends = {
-            dev_repo("saecki/live-rename.nvim"),
+        deps = {
             "williamboman/mason.nvim",
             "hrsh7th/cmp-nvim-lsp",
             "folke/trouble.nvim",
@@ -273,59 +86,53 @@ function M.setup()
     })
 
     -- debugging
-    add("dap", {
+    pm.add("dap", {
         source = "mfussenegger/nvim-dap",
-        depends = {
+        deps = {
             "rcarriga/nvim-dap-ui",
             "nvim-neotest/nvim-nio"
         },
     })
 
     -- treesitter
-    add("treesitter", {
+    pm.add("treesitter", {
         source = "nvim-treesitter/nvim-treesitter",
-        hooks = {
-            post_checkout = function()
-                vim.cmd.TSUpdate()
-            end,
-        },
+        post_checkout = function()
+            vim.cmd.TSUpdate()
+        end,
     })
-    add(nil, dev_repo({ source = "saecki/nvim-treesitter-context", checkout = "categories" }))
-    add(nil, "nvim-treesitter/nvim-treesitter-textobjects")
-    add(nil, "yorickpeterse/nvim-tree-pairs")
+    pm.dev_repo(nil, { source = "saecki/nvim-treesitter-context", checkout = "categories" })
+    pm.add(nil, "nvim-treesitter/nvim-treesitter-textobjects")
+    pm.add(nil, "yorickpeterse/nvim-tree-pairs")
 
     -- markdown
-    add(nil, {
+    pm.add(nil, {
         source = "iamcco/markdown-preview.nvim",
-        hooks = {
-            post_checkout = function()
-                vim.call("mkdp#util#install")
-            end
-        },
+        post_checkout = function()
+            vim.call("mkdp#util#install")
+        end
     })
-    add("markview", "OXY2DEV/markview.nvim")
+    pm.add("markview", "OXY2DEV/markview.nvim")
 
     vim.g.table_mode_toggle_map = "e"
     vim.g.table_mode_tableize_d_map = "<leader>tT"
-    add("table-mode", "dhruvasagar/vim-table-mode")
+    pm.add("table-mode", "dhruvasagar/vim-table-mode")
 
     -- rust
-    add("crates", dev_repo("saecki/crates.nvim"))
+    pm.dev_repo("crates", "saecki/crates.nvim")
 
     -- lua/teal
-    add(nil, "teal-language/vim-teal")
+    pm.add(nil, "teal-language/vim-teal")
 
     -- typst
-    add(nil, "kaarmu/typst.vim")
+    pm.add(nil, "kaarmu/typst.vim")
 
     -- discord rich presence
     vim.g.presence_has_setup = 1
-    add("presence", "andweeb/presence.nvim")
+    pm.add("presence", "andweeb/presence.nvim")
 
     -- run all queued setups
-    for _, name in ipairs(setup_queue) do
-        require(name).setup()
-    end
+    pm.run_queued_setups()
 
     -- some language specific things
     require("config.lang.zig").setup()
@@ -333,13 +140,15 @@ function M.setup()
     require("config.lang.lua").setup()
 
     local wk = require("which-key.config")
-    wk.add({
-        { "<leader>p",  group = "Plugins" },
-        { "<leader>pu", mini_deps.update,        desc = "Update" },
-        { "<leader>ps", "<cmd>DepsSnapSave<cr>", desc = "Save snapshot" },
-        { "<leader>pr", "<cmd>DepsSnapLoad<cr>", desc = "Restore snapshot" },
-        { "<leader>pl", "<cmd>DepsShowLog<cr>",  desc = "Log" },
-    })
+    -- TODO: implement update
+    -- TODO: automatically store a lock file
+    -- TODO: allow restoring to a lock file
+    -- wk.add({
+    --     { "<leader>p",  group = "Plugins" },
+    --     { "<leader>pu", pm.update,  desc = "Update"  },
+    --     { "<leader>pr", pm.restore, desc = "Restore" },
+    --     { "<leader>pl", pm.log,     desc = "Log"     },
+    -- })
 end
 
 return M
---
+++ .config/nvim/lua/plugman.lua
@@ -0,0 +1,312 @@
+local M = {}
+
+local lock_file_path = vim.fn.stdpath("config") .. "/plugs.lock"
+local plugs_path = vim.fn.stdpath("data") .. "/site/pack/plugs/opt/"
+local projects_path = vim.fn.expand("~/Projects/")
+
+local native_notify = vim.notify
+
+local function print_info(pattern, ...)
+    native_notify(string.format(pattern, ...), vim.log.levels.INFO)
+    vim.cmd.redraw()
+end
+
+local function print_error(pattern, ...)
+    native_notify(string.format(pattern, ...), vim.log.levels.ERROR)
+    vim.cmd.redraw()
+end
+
+local function rm_rf(path)
+    local res = nil
+    vim.system({ "rm", "-rf", path }, {}, function(r)
+        res = r
+    end)
+    vim.wait(100, function()
+        return res ~= nil
+    end, 1)
+    if res and res.code == 0 then
+        print_info("removed `%s`", path)
+    else
+        print_error("error removing `%s`: %s", path, res.stderr)
+        error()
+    end
+end
+
+local function unlink(path, prev_link)
+    local ok, err = vim.uv.fs_unlink(path)
+    if ok then
+        print_info("unlinked `%s` -> `%s`", path, prev_link)
+    else
+        print_error("error unlinking `%s` -> `%s`: %s", path, prev_link, err)
+        error()
+    end
+end
+
+local function mkdir(path, mode)
+    local ok, err = vim.uv.fs_mkdir(path, mode)
+    if ok then
+        print_info("created dir (%o) `%s`", mode, path)
+    else
+        print_info("error creating dir (%o) `%s`: `%s`", mode, path, err)
+        error()
+    end
+end
+
+local function symlink(link, link_target)
+    local ok, err = vim.uv.fs_symlink(link_target, link, { dir = true })
+    if ok then
+        print_info("created symlink `%s` -> `%s`", link, link_target)
+    else
+        print_error("error creating symlink from `%s` -> `%s`: %s", link, link_target, err)
+        error()
+    end
+end
+
+---@param command string[]
+---@param opts {cwd: string?}?
+local function run_command(command, opts)
+    opts = opts or {}
+
+    local command_str = table.concat(command, " ")
+    print_info("running `%s`", command_str)
+    vim.cmd.redraw()
+
+    local res = nil
+    local timeout = 30000
+    local command_opts = {
+        timeout = timeout,
+        cwd = opts.cwd,
+    }
+    vim.system(command, command_opts, function(r)
+        res = r
+    end)
+    vim.wait(timeout, function()
+        return res ~= nil
+    end, 100)
+
+    if res and res.code == 0 then
+        print_info("success `%s`", command_str)
+    else
+        print_error("error running `%s`: `%s`", command_str, res.stderr)
+        error()
+    end
+end
+
+local function create_all_dirs(path)
+    if vim.uv.fs_stat(path) then
+        return
+    end
+
+    local components = {}
+    for pos in path:gmatch("/?[^/]+()/?") do
+        table.insert(components, pos)
+    end
+
+    local from = 0
+    for i = #components - 1, 1, -1 do
+        local pos = components[i]
+        local sub_path = string.sub(path, 0, pos)
+
+        if vim.uv.fs_stat(sub_path) then
+            from = i + 1
+            break
+        end
+    end
+
+    for i = from, #components do
+        local pos = components[i]
+        local sub_path = string.sub(path, 0, pos)
+        local mode = 7 * 8 * 8 + 5 * 8 + 5 -- 755
+        mkdir(sub_path, mode)
+    end
+end
+
+---@param path string
+---@return string
+local function plugin_dir_name(path)
+    return string.match(path, "/?([^/]+)$")
+end
+
+---@class PlugSpec
+---@field source string
+---@field checkout string?
+---@field deps (DepSpec|string)[]?
+---@field post_checkout fun()?
+
+---@class DepSpec
+---@field source string
+---@field checkout string?
+
+---@type table<string,DepSpec>
+local plugins = {}
+
+---@param spec PlugSpec|DepSpec
+local function ensure_installed_git_repo(spec)
+    local dir_name = plugin_dir_name(spec.source)
+    local package_path = plugs_path .. dir_name
+    local repo_url = spec.source
+    if string.match(repo_url, "^[^/]+/[^/]+$") then
+        repo_url = "https://github.com/" .. spec.source
+    end
+
+    local cloned = false
+    if not vim.uv.fs_stat(package_path) then
+        run_command({
+            "git", "clone", "--quiet", "--filter=blob:none",
+            "--recurse-submodules", "--also-filter-submodules",
+            "--origin", "origin",
+            repo_url, package_path,
+        })
+        if spec.checkout then
+            run_command({ "git", "checkout", "--quiet", spec.checkout }, { cwd = package_path })
+        end
+        cloned = true
+    end
+
+    plugins[spec.source] = {
+        source = spec.source,
+        checkout = spec.checkout,
+    }
+
+    return cloned
+end
+
+---@type string[]
+local setup_queue = {}
+
+---@param cfg_file string?
+---@param spec PlugSpec
+---@param checkout boolean
+local function add_plugin(cfg_file, spec, checkout)
+    -- dependencies
+    if spec.deps then
+        for _, d in ipairs(spec.deps) do
+            if type(d) == "string" then
+                d = { source = d }
+            end
+            ensure_installed_git_repo(d)
+
+            local dir_name = plugin_dir_name(d.source)
+            vim.cmd.packadd(dir_name)
+        end
+    end
+
+    local dir_name = plugin_dir_name(spec.source)
+    vim.cmd.packadd(dir_name)
+
+    if checkout and spec.post_checkout then
+        spec.post_checkout()
+    end
+
+    if cfg_file then
+        table.insert(setup_queue, "config." .. cfg_file)
+    end
+end
+
+---@param cfg_file string?
+---@param spec PlugSpec|string
+function M.add(cfg_file, spec)
+    if type(spec) == "string" then
+        spec = { source = spec }
+    end
+
+    local cloned = ensure_installed_git_repo(spec)
+
+    add_plugin(cfg_file, spec, cloned)
+end
+
+---@param cfg_file string?
+---@param spec PlugSpec|string
+function M.dev_repo(cfg_file, spec)
+    if type(spec) == "string" then
+        spec = { source = spec }
+    end
+    assert(string.match(spec.source, "^[^/]+/[^/]+$"))
+    local repo_url = "git@github.com:" .. spec.source
+
+    local dir_name = plugin_dir_name(spec.source)
+    local project_path = projects_path .. dir_name
+    local package_path = plugs_path .. dir_name
+
+    -- clone repository into `~/Projects` dir
+    local cloned = false
+    if not vim.uv.fs_stat(project_path) then
+        run_command({ "git", "clone", "--quiet", repo_url, project_path })
+        if spec.checkout then
+            run_command({ "git", "checkout", "--quiet", spec.checkout }, { cwd = project_path })
+        end
+        cloned = true
+    end
+
+    -- symlink it into the plugins dir
+    if vim.uv.fs_stat(package_path) then
+        local link_path = vim.uv.fs_readlink(package_path)
+        if link_path == project_path then
+            -- correct symlink already exists
+        elseif link_path then
+            -- incorrect symlink
+            unlink(package_path, project_path)
+            symlink(package_path, project_path)
+        else
+            -- not a symlink
+            rm_rf(package_path)
+            symlink(package_path, project_path)
+        end
+    else
+        symlink(package_path, project_path)
+    end
+
+    add_plugin(cfg_file, spec, cloned)
+end
+
+function M.create_dirs()
+    create_all_dirs(plugs_path)
+    create_all_dirs(projects_path)
+end
+
+---@param name string?
+---@param keys string[]
+function M.setup_on_keys(name, keys)
+    local loaded = false
+    local function load_and_feedkeys(key)
+        return function()
+            if loaded then
+                return
+            end
+            loaded = true
+
+            -- delete lazy loading keymaps
+            for _, lhs in ipairs(keys) do
+                vim.keymap.del("n", lhs)
+            end
+
+            require("config." .. name).setup()
+
+            local feed = vim.api.nvim_replace_termcodes("<ignore>" .. key, true, true, true)
+            vim.api.nvim_feedkeys(feed, "i", false)
+        end
+    end
+
+    for _, lhs in ipairs(keys) do
+        vim.keymap.set("n", lhs, load_and_feedkeys(lhs), {})
+    end
+end
+
+function M.run_queued_setups()
+    for _, name in ipairs(setup_queue) do
+        local ok, config, err = pcall(require, name)
+        if not ok then
+            vim.notify(string.format("failed to load `%s`:\n%s", name, config))
+            goto continue
+        end
+
+        local ok, res = pcall(config.setup)
+        if not ok then
+            vim.notify(string.format("failed to run setup for `%s`:\n%s", name, res))
+        end
+
+        ::continue::
+    end
+end
+
+return M
