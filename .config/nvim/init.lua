42d14770971c758247e6e7753c17e5cea9c29e0f
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2024-08-20 11:25:31 +0200

    refactor(nvim): simplify init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,32 +1,22 @@
-local function error_handler(name)
-    return function(err)
-        local text = "Failed to load module '" .. name .. "':\n" .. (err or "")
-        vim.notify(text, vim.log.levels.ERROR)
-        return err
-    end
-end
-
-local function prequire(name, setup)
-    local ok, mod = xpcall(function()
-        return require(name)
-    end, error_handler(name))
+local function load_and_setup(name)
+    local ok, mod, err = pcall(require, name)
     if not ok then
+        vim.notify(string.format("failed to load `%s`:\n%s", name, mod))
         return
     end
 
-    if setup ~= false then
-        xpcall(mod.setup, error_handler(name))
+    local ok, res = pcall(mod.setup)
+    if not ok then
+        vim.notify(string.format("failed to run setup for `%s`:\n%s", name, res))
     end
-
-    return mod
 end
 
 vim.loader.enable()
 
-prequire("globals")
-prequire("util.select")
-prequire("options")
-prequire("colors")
-prequire("mappings")
-prequire("diagnostic")
-prequire("plugins")
+load_and_setup("globals")
+load_and_setup("util.select")
+load_and_setup("options")
+load_and_setup("colors")
+load_and_setup("mappings")
+load_and_setup("diagnostic")
+load_and_setup("plugins")
