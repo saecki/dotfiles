801485f2e7417ff7682660947bc3efdf94b7baf5
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2024-08-21 20:13:56 +0200

    fix(nvim): add moved files

---
+++ .config/nvim/lua/lang/rust.lua
@@ -0,0 +1,21 @@
+local util = require("util")
+
+local M = {}
+
+function M.setup()
+    -- make library files readonly
+    local registry_path = vim.fn.expand("$HOME/.cargo/registry/src/**.rs")
+    local rustc_sysroot = vim.trim(util.bash_eval("rustc --print sysroot"))
+    local stdlib_path = rustc_sysroot.."/lib/rustlib/src/**.rs"
+    local group = vim.api.nvim_create_augroup("user.config.lang.rust", {})
+    vim.api.nvim_create_autocmd("BufReadPre", {
+        group = group,
+        pattern = { registry_path, stdlib_path },
+        callback = function(ev)
+            vim.bo[ev.buf].readonly = true
+            vim.bo[ev.buf].modifiable = false
+        end
+    })
+end
+
+return M
