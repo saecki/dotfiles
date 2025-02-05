d8618bd12177916097644992e06f8984e77f2da6
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2025-02-05 14:50:15 +0100

    fix(nvim): add oil config file

---
+++ .config/nvim/lua/config/oil.lua
@@ -0,0 +1,23 @@
+local oil = require("oil")
+local shared = require("shared")
+
+local M = {}
+
+function M.setup()
+    require("oil").setup({
+        float = {
+            border = shared.window.border,
+        },
+        confirmation = {
+            border = shared.window.border,
+        },
+        progress = {
+            border = shared.window.border,
+        },
+        keymaps_help = {
+            border = shared.window.border,
+        },
+    })
+end
+
+return M
