50ac62f4caa1ee5f2cf46d5376d2ce8b4dfeda7f
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2025-03-20 14:08:59 +0100

    chore(nvim): update plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -63,7 +63,7 @@ function M.setup()
     -- completion
     pm.add("blink", {
         source = "Saghen/blink.cmp",
-        checkout = "v0.13.1",
+        checkout = "v0.14.0",
     })
 
     -- git
