65152789b28412a68afac551376cb2a0dd6e955b
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2025-05-11 17:26:35 +0200

    feat(nvim): custom rust_analyzer view memory layout implementation

--- .config/nvim/lua/config/lsp/server/rust_analyzer.lua
+++ .config/nvim/lua/config/lsp/server/rust_analyzer.lua
@@ -4,6 +4,323 @@ local wk = require("which-key.config")
 local M = {}
 
 local use_clippy = false
+---@type integer?
+local split_win = nil
+
+
+---@param method vim.lsp.protocol.Method|string
+---@param params any
+---@param handler lsp.Handler
+local function rust_analyzer_request(method, params, handler)
+    local client = vim.lsp.get_clients({ name = "rust_analyzer" })[1]
+    if not client then
+        vim.notify("rust_analyzer is not attached")
+        return
+    end
+
+    if params == nil then
+        params = vim.lsp.util.make_position_params(0, client.offset_encoding)
+    end
+
+    client:request(method, params, handler)
+end
+
+---@class SplitWinOpts
+---@field below boolean?
+---@field nowrap boolean?
+---@field filetype string?
+
+---@param lines string[]
+---@param opts SplitWinOpts
+local function open_split_win(lines, opts)
+    local buf = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+    vim.bo[buf].filetype = opts.filetype or "rust"
+
+    local split_dir = opts.below and "below" or "right"
+
+    if split_win and vim.api.nvim_win_is_valid(split_win) then
+        vim.api.nvim_win_set_buf(split_win, buf)
+        vim.api.nvim_win_set_config(split_win, {
+            split = split_dir,
+        })
+    else
+        split_win = vim.api.nvim_open_win(buf, false, {
+            win = 0,
+            split = split_dir,
+        })
+    end
+
+    vim.wo[split_win].wrap = not opts.nowrap
+end
+
+local function expand_macro()
+    rust_analyzer_request("rust-analyzer/expandMacro", nil, function(_, result)
+        if result == nil then
+            vim.notify("No expansion")
+            return
+        end
+
+        local lines = vim.split(result.expansion, "\n")
+        table.insert(lines, 1, string.format("// expansion of `%s`", result.name))
+
+        open_split_win(lines, {})
+    end)
+end
+
+local function view_memory_layout()
+    ---@class RaNode
+    ---@field itemName string
+    ---@field typename string
+    ---@field size integer
+    ---@field offset integer
+    ---@field alignment integer
+    ---@field childrenStart integer
+    ---@field childrenLen integer
+    ---@field parentIdx integer
+
+    ---@class Node
+    ---@field name string
+    ---@field type string
+    ---@field size integer
+    ---@field offset integer
+    ---@field align integer
+    ---@field children Node[]?
+    ---@field measured Measured?
+
+    ---@class Measured
+    ---@field height integer
+    ---@field name string
+    ---@field layout string
+
+    ---@class Column
+    ---@field width integer
+    ---@field last_y integer
+
+    ---@param list RaNode[]
+    ---@param ra_node RaNode
+    ---@return Node
+    local function build_tree(list, ra_node)
+        ---@type Node
+        local node = {
+            name = ra_node.itemName,
+            type = ra_node.typename,
+            size = ra_node.size,
+            offset = ra_node.offset,
+            align = ra_node.alignment,
+        }
+        if ra_node.childrenStart == -1 then
+            return node
+        end
+
+        local children = {}
+        local start = ra_node.childrenStart + 1
+        for ra_child in vim.iter(list):slice(start, start + ra_node.childrenLen - 1) do
+            if ra_child.size > 0 then
+                local child = build_tree(list, ra_child)
+                table.insert(children, child)
+            end
+        end
+        table.sort(children, function(a, b)
+            return a.offset < b.offset
+        end)
+        node.children = children
+
+        return node
+    end
+
+    ---@type integer[]
+    local col_widths = {}
+    ---@type integer[]
+    local scalar_offsets = {}
+    ---@type Node[][]
+    local grid = {}
+
+    ---@param x integer
+    ---@param y integer
+    ---@param node Node
+    local function set_grid(x, y, node)
+        local row = grid[x] or {}
+        grid[x] = row
+        row[y] = node
+    end
+
+    ---@param x integer
+    ---@param y integer
+    ---@return Node?
+    local function get_grid(x, y)
+        local row = grid[x] or {}
+        return row[y]
+    end
+
+    ---@param x integer
+    ---@param y integer
+    ---@param parent_offset integer
+    ---@param node Node
+    ---@return Measured
+    local function measure_tree(x, y, parent_offset, node)
+        set_grid(x, y, node)
+
+        local name = node.name .. ": " .. node.type
+        local layout = string.format("size: %s, align: %s", node.size, node.align)
+
+        local offset = parent_offset + node.offset
+        local width = col_widths[x + 1] or 0
+        col_widths[x + 1] = math.max(width, name:len(), layout:len())
+
+        if not node.children then
+            table.insert(scalar_offsets, offset)
+            node.measured = {
+                height = 1,
+                name = name,
+                layout = layout,
+            }
+            return node.measured
+        end
+
+        local height = 0
+        local y_offset = 0
+        for _, child in ipairs(node.children) do
+            local child_y = y + y_offset
+            local mes = measure_tree(x + 1, child_y, offset, child)
+            height = height + mes.height
+            y_offset = y_offset + mes.height
+        end
+        node.measured = {
+            height = height,
+            name = name,
+            layout = layout,
+        }
+        return node.measured
+    end
+
+    local line_builder = {}
+    ---@param idx integer
+    ---@param str string
+    local function push_line(idx, str)
+        local builder = line_builder[idx] or {}
+        table.insert(builder, str)
+        line_builder[idx] = builder
+    end
+
+    local cross = "┼"
+    local vert = "│"
+    local hor = "─"
+
+    ---@param x integer 0-based
+    ---@param y integer 0-based
+    ---@param node Node
+    local function render_tree(x, y, node)
+        local width = col_widths[x + 1]
+        local mes = assert(node.measured)
+
+        do
+            -- draw cell
+            local line_idx = 3 * y + 1
+            push_line(line_idx, cross)
+            push_line(line_idx, string.rep(hor, width))
+
+            push_line(line_idx + 1, vert)
+            push_line(line_idx + 1, mes.name)
+            push_line(line_idx + 1, string.rep(" ", width - mes.name:len()))
+
+            push_line(line_idx + 2, vert)
+            push_line(line_idx + 2, mes.layout)
+            push_line(line_idx + 2, string.rep(" ", width - mes.layout:len()))
+
+            -- draw end line
+            if not node.children then
+                push_line(line_idx, cross)
+                push_line(line_idx + 1, vert)
+                push_line(line_idx + 2, vert)
+                return
+            end
+        end
+
+        -- add padding for other rows
+        for i = 1, mes.height - 1 do
+            local line_idx = 3 * (y + i) + 1
+            push_line(line_idx, cross)
+            push_line(line_idx, string.rep(" ", width))
+
+            push_line(line_idx + 1, vert)
+            push_line(line_idx + 1, string.rep(" ", width))
+
+            push_line(line_idx + 2, vert)
+            push_line(line_idx + 2, string.rep(" ", width))
+        end
+
+        local y_offset = 0
+        for _, child in ipairs(node.children) do
+            local child_y = y + y_offset
+            local child_x = x + 1
+            render_tree(child_x, child_y, child)
+            local child_mes = assert(child.measured)
+            y_offset = y_offset + child_mes.height
+        end
+    end
+
+    ---@param x integer 0-based
+    ---@param y integer 0-based
+    ---@param node Node
+    local function close_tree_cells(x, y, node)
+        local width = col_widths[x + 1]
+        local mes = assert(node.measured)
+
+        -- draw bottom line
+        local close_bottom = get_grid(x, y + mes.height) == nil
+        if close_bottom then
+            local line_idx = 3 * (y + mes.height) + 1
+            push_line(line_idx, string.rep(hor, width))
+            push_line(line_idx, cross)
+        end
+
+        if not node.children then
+            return
+        end
+
+        local y_offset = 0
+        for _, child in ipairs(node.children) do
+            local child_y = y + y_offset
+            local child_x = x + 1
+            close_tree_cells(child_x, child_y, child)
+            local child_mes = assert(child.measured)
+            y_offset = y_offset + child_mes.height
+        end
+    end
+
+    rust_analyzer_request("rust-analyzer/viewRecursiveMemoryLayout", nil, function(_, result)
+        if result == nil then
+            vim.notify("No memory layout")
+            return
+        end
+
+        local tree = build_tree(result.nodes, result.nodes[1])
+
+        -- measure
+        measure_tree(0, 0, 0, tree)
+
+        -- render
+        for i, offset in ipairs(scalar_offsets) do
+            local idx = 3 * (i - 1) + 1
+            push_line(idx, string.format("%4d ", offset))
+            push_line(idx + 1, "     ")
+            push_line(idx + 2, "     ")
+        end
+        push_line(#line_builder + 1, string.format("%4d %s", tree.size, cross))
+        render_tree(0, 0, tree)
+        close_tree_cells(0, 0, tree)
+
+        -- display
+        local lines = vim.iter(line_builder)
+            :map(function(l) return table.concat(l) end)
+            :totable()
+        open_split_win(lines, {
+            below = true,
+            nowrap = true,
+        })
+    end)
+end
 
 local function setup_server(opts)
     opts = opts or {}
@@ -50,11 +367,11 @@ function M.setup()
             wk.add({
                 buffer = ev.buf,
                 { "<leader>ic",  toggle_check_command,               desc = "Rust check command" },
-                { "<leader>ie",  "<cmd>FerrisExpandMacro<cr>",       desc = "Rust expand" },
+                { "<leader>ie",  expand_macro,                       desc = "Rust expand" },
 
                 { "<leader>ivh", "<cmd>FerrisViewHIR<cr>",           desc = "Rust view HIR" },
                 { "<leader>ivm", "<cmd>FerrisViewMIR<cr>",           desc = "Rust view MIR" },
-                { "<leader>ivl", "<cmd>FerrisViewMemoryLayout<cr>",  desc = "Rust view memory layout" },
+                { "<leader>ivl", view_memory_layout,                 desc = "Rust view memory layout" },
                 -- TODO: support new viewSyntaxTree extension method
                 -- { "<leader>ivs", "<cmd>FerrisViewSyntaxTree<cr>",    desc = "Rust view syntax tree",  mode = { "n", "v" } },
                 { "<leader>ivi", "<cmd>FerrisViewItemTree<cr>",      desc = "Rust view item tree" },
