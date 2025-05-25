dde6e1e5c19366453ffc74013d9b5b915e7e257e
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2025-05-25 14:12:16 +0200

    feat(nvim): set zathura as typst watch default pdf viewer

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -127,6 +127,7 @@ function M.setup()
 
     -- typst
     vim.g.typst_auto_open_quickfix = 0
+    vim.g.typst_pdf_viewer = "zathura"
     pm.add(nil, "kaarmu/typst.vim")
     pm.dev_repo("typst-test-helper", "saecki/typst-test-helper.nvim")
 
