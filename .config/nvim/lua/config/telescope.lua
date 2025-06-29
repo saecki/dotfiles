3bd596000bb0f6b3e0956cded9f4b1ec423ca876
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2025-06-29 23:36:50 +0200

    fix(nvim): use telescope.lsp_dynamic_workspace_symbols to avoid rust-analyzer search limit

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -33,6 +33,19 @@ local symbol_highlights = {
     ["TypeParameter"] = "TelescopeLspSymbolKindTypeParameter",
 }
 
+local function document_symbols()
+    telescope_builtin.lsp_document_symbols({
+        symbol_highlights = symbol_highlights,
+    })
+end
+
+local function workspace_symbols()
+    telescope_builtin.lsp_dynamic_workspace_symbols({
+        symbol_highlights = symbol_highlights,
+    })
+end
+
+
 local function find_files(opts)
     return function()
         local fd_cmd = { "fd", "--type", "f", "--exclude", ".git", "--hidden" }
@@ -102,26 +115,23 @@ function M.setup()
         },
     })
 
-    local symbol_opts = {
-        symbol_highlights = symbol_highlights,
-    }
     wk.add({
         { "<leader>f",  group = "Find" },
-        { "<leader>fp", find_files({ no_ignore = false }),                                   desc = "Files" },
-        { "<leader>fP", find_files({ no_ignore = true }),                                    desc = "Ignored Files" },
-        { "<leader>ff", telescope_builtin.live_grep,                                         desc = "Live grep" },
-        { "<leader>fb", telescope_builtin.buffers,                                           desc = "Buffers" },
-        { "<leader>fh", telescope_builtin.help_tags,                                         desc = "Help" },
-        { "<leader>fc", telescope_builtin.commands,                                          desc = "Commands" },
-        { "<leader>fm", telescope_builtin.keymaps,                                           desc = "Key mappings" },
-        { "<leader>fi", telescope_builtin.highlights,                                        desc = "Highlight groups" },
-        { "<leader>fg", telescope_builtin.git_status,                                        desc = "Git status" },
-        { "<leader>fd", buf_diagnostics,                                                     desc = "Document diagnostics" },
-        { "<leader>fD", telescope_builtin.diagnostics,                                       desc = "Workspace diagnostics" },
-        { "<leader>fs", function() telescope_builtin.lsp_document_symbols(symbol_opts) end,  desc = "LSP document symbols" },
-        { "<leader>fS", function() telescope_builtin.lsp_workspace_symbols(symbol_opts) end, desc = "LSP workspace symbols" },
-        { "<leader>fw", trailing_whitespace,                                                 desc = "Whitespace", },
-        { "<leader>fr", telescope_builtin.resume,                                            desc = "Resume" },
+        { "<leader>fp", find_files({ no_ignore = false }), desc = "Files" },
+        { "<leader>fP", find_files({ no_ignore = true }),  desc = "Ignored Files" },
+        { "<leader>ff", telescope_builtin.live_grep,       desc = "Live grep" },
+        { "<leader>fb", telescope_builtin.buffers,         desc = "Buffers" },
+        { "<leader>fh", telescope_builtin.help_tags,       desc = "Help" },
+        { "<leader>fc", telescope_builtin.commands,        desc = "Commands" },
+        { "<leader>fm", telescope_builtin.keymaps,         desc = "Key mappings" },
+        { "<leader>fi", telescope_builtin.highlights,      desc = "Highlight groups" },
+        { "<leader>fg", telescope_builtin.git_status,      desc = "Git status" },
+        { "<leader>fd", buf_diagnostics,                   desc = "Document diagnostics" },
+        { "<leader>fD", telescope_builtin.diagnostics,     desc = "Workspace diagnostics" },
+        { "<leader>fs", document_symbols,                  desc = "LSP document symbols" },
+        { "<leader>fS", workspace_symbols,                 desc = "LSP workspace symbols" },
+        { "<leader>fw", trailing_whitespace,               desc = "Whitespace", },
+        { "<leader>fr", telescope_builtin.resume,          desc = "Resume" },
     })
 end
 
