84899337007f584406e8be9c5d01a1d92b76d3db
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2025-07-09 09:33:20 +0200

    feat(nvim): change key to preview quickfix item

--- .config/nvim/ftplugin/qf.lua
+++ .config/nvim/ftplugin/qf.lua
@@ -1,4 +1,4 @@
 vim.wo.wrap = false
 local buf = vim.api.nvim_get_current_buf()
 vim.keymap.set("n", "o", "<cr><cmd>cclose<cr>", { desc = "jump close", buffer = buf })
-vim.keymap.set("n", "p", "<cr><c-w>p", { desc = "preview", buffer = buf })
+vim.keymap.set("n", "K", "<cr><c-w>p", { desc = "preview", buffer = buf })
