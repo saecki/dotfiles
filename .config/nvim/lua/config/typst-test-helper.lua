ba370e6d6404fcbec4dd7b0ea656066a849d02d4
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2025-07-30 14:15:22 +0200

    feat(nvim): update typst-test-helper key mappings

--- .config/nvim/lua/config/typst-test-helper.lua
+++ .config/nvim/lua/config/typst-test-helper.lua
@@ -1,13 +1,18 @@
 local tth = require("typst-test-helper")
+local wk = require("which-key.config")
 
 local M = {}
 
 local function on_attach(buf)
-    local function opts(desc)
-        return { buffer = buf, desc = desc }
-    end
-    vim.keymap.set("n", "<leader>iot", tth.map_open("identity"), opts("Open typst test"))
-    vim.keymap.set("n", "<leader>iog", tth.map_open("geeqie"), opts("Open typst test"))
+    wk.add({
+        buffer = buf,
+        { "<leader>it",  group = "Typst test" },
+        { "<leader>itt", tth.run_test,                    desc = "Run test" },
+        { "<leader>itu", tth.update_ref,                  desc = "Update ref" },
+        { "<leader>itr", tth.map_open_render("identity"), desc = "Open render" },
+        { "<leader>ith", tth.open_html,                   desc = "Open html" },
+        { "<leader>itp", tth.open_pdftags,                desc = "Open pdftags" },
+    })
 end
 
 function M.setup()
