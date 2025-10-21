c42b0599e4afaff2f843b5fe7ccc4641ca39dc56
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2025-10-21 12:54:58 +0200

    refactor(nvim): update typst-test-helper key mappings

--- .config/nvim/lua/config/typst-test-helper.lua
+++ .config/nvim/lua/config/typst-test-helper.lua
@@ -7,9 +7,9 @@ local function on_attach(buf)
     wk.add({
         buffer = buf,
         { "<leader>it",  group = "Typst test" },
-        { "<leader>itt", tth.map_run_test(),                desc = "Run test" },
-        { "<leader>its", tth.map_run_test({ "--scale=4" }), desc = "Run test scale=4" },
-        { "<leader>itu", tth.map_run_test({ "--update" }),  desc = "Update ref" },
+        { "<leader>itt", tth.map_run(),                desc = "Run test" },
+        { "<leader>its", tth.map_run({ "--scale=4" }), desc = "Run test scale=4" },
+        { "<leader>itu", tth.map_run({ "--update" }),  desc = "Update ref" },
         { "<leader>itr", tth.map_open_render("identity"),   desc = "Open render" },
         { "<leader>ith", tth.open_html,                     desc = "Open html" },
         { "<leader>itp", tth.open_pdftags,                  desc = "Open pdftags" },
