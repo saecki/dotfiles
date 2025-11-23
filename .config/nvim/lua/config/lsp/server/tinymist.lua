69b0f4909fc3924bf3e27fd563c6aaeb1e6f0cff
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2025-11-23 22:09:50 +0100

    feat(nvim): allow toggling tinymist html export

--- .config/nvim/lua/config/lsp/server/tinymist.lua
+++ .config/nvim/lua/config/lsp/server/tinymist.lua
@@ -1,10 +1,40 @@
+local wk = require("which-key.config")
+
 local M = {}
 
-function M.setup()
+local html_export = false
+
+local function setup_server(opts)
+    opts = opts or {}
+    local target = opts.export_target or nil -- defaults to paged export
     vim.lsp.config("tinymist", {
         settings = {
             formatterMode = "typstyle",
+            exportTarget = target,
         },
+    })
+end
+
+local function toggle_export_target()
+    html_export = not html_export
+    local target = html_export and "html" or "paged"
+    setup_server({ export_target = target })
+    vim.notify("tinymist target: " .. target, vim.log.levels.INFO)
+end
+
+function M.setup()
+    setup_server()
+
+    local group = vim.api.nvim_create_augroup("user.config.lsp.server.tinymist", {})
+    vim.api.nvim_create_autocmd("BufRead", {
+        group = group,
+        pattern = "*.typ",
+        callback = function(ev)
+            wk.add({
+                buffer = ev.buf,
+                { "<leader>ie", toggle_export_target, desc = "Tinymist html export" },
+            })
+        end
     })
 end
 
