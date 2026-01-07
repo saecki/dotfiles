8759b7749edca94a5c23a2c0a9c2ae3c134e2b2c
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2026-01-07 16:50:56 +0100

    refactor(nvim): comment keymappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -49,31 +49,34 @@ function M.setup()
     -- TODO: make tmux/kitty send different escape sequences for <tab> and <c-i> etc.
     -- vim.keymap.set("", "<tab>", "<c-i>", {})
 
-    vim.keymap.set("", "<c-RightMouse>", "<c-o>", { desc = "Jump back" })
-    vim.keymap.set("", "<a-RightMouse>", "<c-o>", { desc = "Jump back" })
-
+    -- wrapped lines
     vim.keymap.set("", "j", "gj", { silent = true })
     vim.keymap.set("", "gj", "j", { silent = true })
     vim.keymap.set("", "k", "gk", { silent = true })
     vim.keymap.set("", "gk", "k", { silent = true })
 
-    vim.keymap.set({ "n", "i" }, "<f1>", "<esc>")
-    vim.keymap.set("n", "<leader><leader>", "<c-^>", { desc = "Goto previous buffer" })
-    vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Write" })
+    -- jump back
+    vim.keymap.set("", "<c-RightMouse>", "<c-o>", { desc = "Jump back" })
+    vim.keymap.set("", "<a-RightMouse>", "<c-o>", { desc = "Jump back" })
 
+    -- resize windows
     vim.keymap.set("n", "<c-left>", "<cmd>vertical resize -5<cr>", { desc = "Decrease width" })
     vim.keymap.set("n", "<c-right>", "<cmd>vertical resize +5<cr>", { desc = "Increase width" })
     vim.keymap.set("n", "<c-up>", "<cmd>resize -5<cr>", { desc = "Decrease height" })
     vim.keymap.set("n", "<c-down>", "<cmd>resize +5<cr>", { desc = "Increase height" })
 
+    -- toggle things
     vim.keymap.set("n", "<leader>er", "<cmd>set rnu!<cr>", { desc = "Relative line numbers" })
     vim.keymap.set("n", "<leader>el", "<cmd>set list!<cr>", { desc = "Listchars" })
     vim.keymap.set("n", "<leader>es", "<cmd>set spell!<cr>", { desc = "Spellchecking" })
     vim.keymap.set("n", "<leader>edv", toggle_diff, { desc = "View" })
     vim.keymap.set("n", "<leader>edw", toggle_diffopt("iwhite"), { desc = "iwhite" })
 
+    -- misc
+    vim.keymap.set({ "n", "i" }, "<f1>", "<esc>")
+    vim.keymap.set("n", "<leader><leader>", "<c-^>", { desc = "Goto previous buffer" })
+    vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Write" })
     vim.keymap.set("n", "<leader>q", toggle_quickfix, { desc = "Toggle quickfix list" })
-
     vim.keymap.set("v", "<c-c>", '"+y', { desc = "Copy to system clipboard" })
 end
 
