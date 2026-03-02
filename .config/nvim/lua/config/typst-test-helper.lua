48998759510c0afc93fb0815d4e85170cf0dc90e
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2026-03-02 11:01:32 +0100

    feat(nvim): update typst-test-helper key mappings

--- .config/nvim/lua/config/typst-test-helper.lua
+++ .config/nvim/lua/config/typst-test-helper.lua
@@ -11,9 +11,9 @@ local function on_attach(buf)
         { "<leader>its", tth.map_run_this({ "--scale=4" }),    desc = "Run test scale=4" },
         { "<leader>itu", tth.map_run_this({ "--update" }),     desc = "Update ref" },
         { "<leader>itr", tth.map_open_render("identity"), desc = "Open render" },
-        { "<leader>ith", tth.open_html,                   desc = "Open html" },
-        { "<leader>itp", tth.open_pdftags,                desc = "Open pdftags" },
-        { "<leader>itv", tth.open_svg,                    desc = "Open svg" },
+        { "<leader>ith", tth.read_html,                   desc = "Read html" },
+        { "<leader>itp", tth.read_pdftags,                desc = "Read pdftags" },
+        { "<leader>itv", tth.read_svg,                    desc = "Read svg" },
     })
 end
 
